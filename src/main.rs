adb8ed992b8d285d44cd1359518d7511009b14c4
dreamerlzl <875350879@qq.com>
2022-02-19 23:47:47 +0800

    refactor: upgrade clap

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,38 @@
-use std::env;
-use log::debug;
 use anyhow::Context;
 use env_logger::Env;
+use std::env;
 
 mod taskdb;
 
-use clap::{Clap, AppSettings};
+use clap::{Parser, Subcommand};
 
 use crate::taskdb::{print_subtasks, prompt_subtask, prompt_task};
 
-#[derive(Clap)]
-#[clap(setting = AppSettings::ColoredHelp)]
-struct Opts{
+#[derive(Parser, Debug)]
+struct Opts {
     #[clap(short, long)]
     verbose: bool,
 
     #[clap(short, long)]
-    task_id: Option<u32>,  // the task id
+    task_id: Option<u32>, // the task id
 
     #[clap(subcommand)]
     subcmd: SubCommand,
 }
 
-#[derive(Clap)]
-enum SubCommand{
-    List(List),
-    End(End),
-    Add(Add),
-}
-
-#[derive(Debug)]
-#[derive(Clap)]
-struct Add{
-    desc: String,
-    #[clap(short, long)]
-    link: Option<String>,
-}
-
-
-#[derive(Debug)]
-#[derive(Clap)]
-struct List{
-    pattern: Option<String>,
-}
-
-#[derive(Debug)]
-#[derive(Clap)]
-struct End{
-    id_or_order: u32,
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    List {
+        pattern: Option<String>,
+    },
+    End {
+        id_or_order: u32,
+    },
+    Add {
+        desc: String,
+        #[clap(short, long)]
+        link: Option<String>,
+    },
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -67,47 +52,39 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut db = taskdb::open(&db_path)?;
 
     match opts.subcmd {
-        SubCommand::Add(add) => {
+        SubCommand::Add { desc, link } => {
             // add a new task
-            debug!("{:?}", add);
             if let Some(id) = opts.task_id {
-                db.add_subtask(id, add.desc, add.link)?;
+                db.add_subtask(id, desc, link)?;
             } else {
-                db.add_task(add.desc, add.link)?;
+                db.add_task(desc, link)?;
             }
         }
-        SubCommand::List(list) => {
-            debug!("{:?}", list);
+        SubCommand::List { pattern } => {
             if let Some(id) = opts.task_id {
                 let subtasks = db.get_subtasks(id)?;
                 prompt_subtask(id);
-                subtasks
-                .iter()
-                .for_each(|subtask| println!("{}", subtask));
+                subtasks.iter().for_each(|subtask| println!("{}", subtask));
                 // log output
             } else {
-                let tasks = db.get_tasks(list.pattern)?;
+                let tasks = db.get_tasks(pattern)?;
                 prompt_task();
-                tasks
-                .iter()
-                .for_each(|t| {
+                tasks.iter().for_each(|t| {
                     println!("{}", t);
-                    print_subtasks(
-                        db.get_subtasks(t.id).unwrap(), 
-                        1,
-                    ).iter().for_each(|st| {
-                        println!("{}", st);
-                    });
+                    print_subtasks(db.get_subtasks(t.id).unwrap(), 1)
+                        .iter()
+                        .for_each(|st| {
+                            println!("{}", st);
+                        });
                 });
             }
         }
-        SubCommand::End(end) => {
-            debug!("{:?}", end);
+        SubCommand::End { id_or_order } => {
             if let Some(t) = opts.task_id {
-                let order = end.id_or_order;
+                let order = id_or_order;
                 db.remove_subtask(t, order)?;
             } else {
-                let id = end.id_or_order;
+                let id = id_or_order;
                 db.remove_task(id)?;
             }
         }
